4f3c578fca03fa1d139d8201dc1a40415c280666
joao-colen <joaovictorcolen17@gmail.com>
2018-01-24 15:43:55 -0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const client = new ApiAi.ApiAiClient({ accessToken: '639e715963e14f4e886e9fb8cee23e2d' })
 
 var Mony = function () {
-  let accessToken, myID, responseCallback, actionCallback
+  let accessToken, myID, storeID, responseCallback, actionCallback
 
   let sendRequest = function (promise, callback) {
     promise
@@ -13,37 +13,63 @@ var Mony = function () {
     // response from dialofflow
     function handleResponse (serverResponse) {
       let method, body, endpoint
+      // intent name
       let intent = serverResponse.metadata.intentName
 
       switch (intent) {
-        case 'criar produtos':
+        case 'produtos.criar - name - yes - sku - yes':
           endpoint = '/products.json'
+          method = 'POST'
+          body = {
+            'sku': serverResponse.parameters.sku,
+            'name': serverResponse.parameters.name
+          }
           break
-        case 'editar produtos':
+        case 'cadastro.de.login.por.rede.social':
+          let redesocial = serverResponse.parameters.redesocial
+          let promise = client.textRequest('Como criar login pelo ' + redesocial + ' ?')
+          sendRequest(promise)
+          break
+        // case 'editar produtos':
+        //   endpoint = '/products/' + serverResponse.parameters.id + '.json'
+        //   method = 'PATCH'
+        //   break
         case 'deletar produtos':
           endpoint = '/products/' + serverResponse.parameters.id + '.json'
+          method = 'DELETE'
           break
+        // case 'perguntas produtos':
+        //   method = 'GET'
+        //   break
         default:
-      }
-      // call with AJAX
-      let ajax = new XMLHttpRequest()
-      let url = 'https://api.e-com.plus/v1' + endpoint
-      ajax.open(method, url, true)
-      ajax.setRequestHeader('X-Access-Token', accessToken)
-      ajax.setRequestHeader('X-My-ID', myID)
+          // the answer for dialogflow
+          for (var i = 0; i < serverResponse.result.fulfillment.messages.length; i++) {
 
-      if (body) {
-        // send JSON body
-        ajax.send(JSON.stringify(body))
-      } else {
-        ajax.send()
+          }
       }
 
-      ajax.onreadystatechange = function () {
-        if (this.readyState === 4) {
-          // request finished and response is ready
-          if (this.status !== 200) {
-            // try to resend request
+      if (endpoint) {
+        // call with AJAX
+        let ajax = new XMLHttpRequest()
+        let url = 'https://sandbox.e-com.plus/v1/' + endpoint
+        ajax.open(method, url, true)
+        ajax.setRequestHeader('X-Access-Token', accessToken)
+        ajax.setRequestHeader('X-My-ID', myID)
+        ajax.setRequestHeader('X-Store-ID', storeID)
+        ajax.setRequestHeader('Content-Type', 'application/json')
+
+        if (body) {
+          // send JSON body
+          ajax.send(JSON.stringify(body))
+        } else {
+          ajax.send()
+        }
+
+        ajax.onreadystatechange = function () {
+          if (this.readyState === 4) {
+            // request finished and response is ready
+            // TREAT ERROR
+            // responseCallback(serverResponse.result.fulfillment.speech)
           }
         }
       }
@@ -59,10 +85,11 @@ var Mony = function () {
   return {
 
     // function to init conversation on dialogflow with some parameters
-    'init': function (storeID, storeName, domain, name, gender, email, userID, language, token, id, ResponseCallback, ActionCallback) {
+    'init': function (storeid, storeName, domain, name, gender, email, userID, language, token, id, ResponseCallback, ActionCallback) {
       // set token and id to authentication requests
       accessToken = token
       myID = id
+      storeID = storeid
 
       // msg
       responseCallback = ResponseCallback
